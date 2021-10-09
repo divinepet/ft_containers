8e402b97a6e0461a0eea1bec88800c763611e660
Elidia Laronda <elaronda@ev-n6.kzn.21-school.ru>
2021-10-09 21:57:20 +0300

    refresh

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <map>
+
+using std::cout;
+using std::endl;
 
 typedef int T;                  /* type of item to be stored */
 #define compLT(a,b) (a < b)
@@ -285,10 +289,88 @@ Node *findNode(T data) {
 		return(0);
 }
 
+void printElem(Node *t) {
+	if (t->left != NIL)
+		printElem(t->left);
+	cout << t->data << endl;
+	if (t->right != NIL)
+		printElem(t->right);
+}
+
+Node* begin;
+
+Node* get_begin(Node *t) {
+	if (t->left != NIL)
+		get_begin(t->left);
+	else
+		begin = t;
+	return NIL;
+}
+
+Node* increment(Node *t) {
+	if (t->right != NIL) {
+		t = t->right;
+		while (t->left != NIL)
+			t = t->left;
+		return t;
+	}
+	T value = t->data;
+	t = t->parent;
+	while (value > t->data) {
+		t = t->parent;
+	}
+	return t;
+}
+
 int main(int argc, char **argv) {
 	Node *t;
 
-	insertNode(4);
-	std::cout << findNode(4)->data << std::endl;
+	t = insertNode(27);
+	t = insertNode(13);
+	t = insertNode(1);
+	t = insertNode(22);
+	t = insertNode(17);
+	t = insertNode(25);
+	t = insertNode(11);
+	t = insertNode(6);
+	t = insertNode(15);
+	t = insertNode(8);
+
+//	printElem(root);
+	get_begin(root);
+	Node* x = increment(findNode(6));
+	cout << x->data << endl; // 8
+	Node* xx = increment(x);
+	cout << xx->data << endl; // 11
+	Node* xxx = increment(xx);
+	cout << xxx->data << endl; // 13
+	Node* xxxx = increment(xxx);
+	cout << xxxx->data << endl; // 13
+	Node* xxxxx = increment(xxxx);
+	cout << xxxxx->data << endl; // 13
+	Node* xxxxxx = increment(xxxxx);
+	cout << xxxxxx->data << endl; // 13
+	Node* xxxxxxx = increment(xxxxxx);
+	cout << xxxxxxx->data << endl; // 13
+	Node* xxxxxxxx = increment(xxxxxxx);
+	cout << xxxxxxxx->data << endl; // 13
+
+
+
+//	std::map<int, char> mp;
+//	mp.insert(std::make_pair(13, 'e'));
+//	mp.insert(std::make_pair(6, 'e'));
+//	mp.insert(std::make_pair(3, 'e'));
+//	mp.insert(std::make_pair(5, 'e'));
+//	mp.insert(std::make_pair(36, 'e'));
+//	mp.insert(std::make_pair(35, 'e'));
+//	mp.insert(std::make_pair(23, 'e'));
+//	mp.insert(std::make_pair(11, 'e'));
+//
+//	std::map<int, char>::iterator it;
+//
+//	for (it=mp.begin(); it != mp.end(); it++)
+//		std::cout << "key: " << it->first << " value: " << it->second << "\n";
+
 
 }
