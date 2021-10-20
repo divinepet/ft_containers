b3e1b376fe7eb1e94666dd2280fca632533bfb39
Erik <59801674+divinepet@users.noreply.github.com>
2021-10-21 01:02:45 +0300

    Update Iterator.hpp

--- Iterator.hpp
+++ Iterator.hpp
@@ -30,39 +30,39 @@ namespace ft {
 	private:
 		T value;
 	public:
-		typedef T															iterator_type;
+		typedef T								iterator_type;
 		typedef typename iterator_traits<iterator_type>::iterator_category	iterator_category;
 		typedef typename iterator_traits<iterator_type>::value_type       	value_type;
 		typedef typename iterator_traits<iterator_type>::difference_type  	difference_type;
 		typedef typename iterator_traits<iterator_type>::pointer          	pointer;
 		typedef typename iterator_traits<iterator_type>::reference        	reference;
 
-		iterator(T value = NULL) : value(value)				{}
-		~iterator()											{};
+		iterator(T value = NULL) : value(value)					{}
+		~iterator()								{};
 		template <class U> iterator(const iterator<U>& other,
-				typename ft::enable_if<std::is_convertible<U, iterator_type>::value>::type* = 0)
-						: value(other.base()) 				{};
-		T			base() const 							{ return value; }
-		iterator	&operator=(const iterator &obj) 		{ this->value = obj.value; return *this; }
-		iterator	operator++(int)							{ iterator tmp(*this); value++; return tmp; }
-		iterator	operator--(int) 						{ iterator tmp(*this); value--; return tmp; }
-		iterator	&operator++() 							{ value++; return *this; }
-		iterator	&operator--() 							{ value--; return *this; }
-		int			operator-(iterator const &obj) const 	{ return value - obj.value; }
-		int			operator+(iterator const &obj) const 	{ return value + obj.value; }
+			typename ft::enable_if<std::is_convertible<U, iterator_type>::value>::type* = 0)
+				: value(other.base()) 					{};
+		T		base() const 						{ return value; }
+		iterator	&operator=(const iterator &obj) 			{ this->value = obj.value; return *this; }
+		iterator	operator++(int)						{ iterator tmp(*this); value++; return tmp; }
+		iterator	operator--(int) 					{ iterator tmp(*this); value--; return tmp; }
+		iterator	&operator++() 						{ value++; return *this; }
+		iterator	&operator--() 						{ value--; return *this; }
+		int		operator-(iterator const &obj) const 			{ return value - obj.value; }
+		int		operator+(iterator const &obj) const 			{ return value + obj.value; }
 		iterator	operator-(int n) const 					{ return iterator(this->value - n); }
 		iterator	operator+(int n) const 					{ return iterator(this->value + n); }
-		iterator	&operator-=(int n) 						{ this->value -= n; return (*this); }
-		iterator	&operator+=(int n) 						{ this->value += n; return (*this); }
-		reference 	operator*() const 						{ return *value; }
-		pointer 	operator->() const 						{ return &(*value); }
-		reference 	operator[](const unsigned int index) 	{ return *this->value[index]; }
-		bool		operator==(iterator const &obj) const 	{ return value == obj.value; };
-		bool		operator!=(iterator const &obj) const 	{ return value != obj.value; };
-		bool 		operator<(iterator const &obj) const 	{ return value < obj.value; };
-		bool 		operator>(iterator const &obj) const 	{ return value > obj.value; };
-		bool 		operator<=(iterator const &obj) const 	{ return value <= obj.value; };
-		bool 		operator>=(iterator const &obj) const 	{ return value >= obj.value; };
+		iterator	&operator-=(int n) 					{ this->value -= n; return (*this); }
+		iterator	&operator+=(int n) 					{ this->value += n; return (*this); }
+		reference 	operator*() const 					{ return *value; }
+		pointer 	operator->() const 					{ return &(*value); }
+		reference 	operator[](const unsigned int index) 			{ return *this->value[index]; }
+		bool		operator==(iterator const &obj) const 			{ return value == obj.value; };
+		bool		operator!=(iterator const &obj) const 			{ return value != obj.value; };
+		bool 		operator<(iterator const &obj) const 			{ return value < obj.value; };
+		bool 		operator>(iterator const &obj) const 			{ return value > obj.value; };
+		bool 		operator<=(iterator const &obj) const 			{ return value <= obj.value; };
+		bool 		operator>=(iterator const &obj) const 			{ return value >= obj.value; };
 	};
 
 
@@ -78,32 +78,32 @@ namespace ft {
 		typedef typename iterator_traits<iterator_type>::pointer          	pointer;
 		typedef typename iterator_traits<iterator_type>::reference        	reference;
 
-		reverse_iterator(T value = NULL) : value(value)						{}
-		~reverse_iterator() 												{};
+		reverse_iterator(T value = NULL) : value(value)				{}
+		~reverse_iterator() 							{};
 		template <class U> reverse_iterator(const iterator<U>& other,
-				typename ft::enable_if<std::is_convertible<U, iterator_type>::value>::type* = 0)
-						: value(other.base()) 								{};
-		T					base() const 									{ return value; }
-		reverse_iterator	&operator=(const reverse_iterator &obj) 		{ value = obj.value; return *this; }
-		reverse_iterator	operator++(int) 								{ reverse_iterator tmp(*this); value--; return tmp; }
-		reverse_iterator	operator--(int) 								{ reverse_iterator tmp(*this); value++; return tmp; }
-		reverse_iterator	&operator++() 									{ value--; return *this; }
-		reverse_iterator	&operator--() 									{ value++; return *this; }
-		int					operator-(reverse_iterator const &obj) const 	{ return value + obj.value; }
-		int					operator+(reverse_iterator const &obj) const 	{ return value - obj.value; }
-		reverse_iterator	operator-(int n) const 							{ return reverse_iterator(this->value + n); }
-		reverse_iterator	operator+(int n) const 							{ return reverse_iterator(this->value - n); }
-		reverse_iterator	&operator-=(int n) 								{ this->value += n; return (*this); }
-		reverse_iterator	&operator+=(int n) 								{ this->value -= n; return (*this); }
-		reference 			operator*() const 								{ return *value; }
-		pointer 			operator->() const 								{ return &(*value); }
-		reference 			operator[](const unsigned int index) 			{ return *this->value[index]; }
-		bool				operator== (reverse_iterator const &obj) const 	{ return value == obj.value; };
-		bool				operator!= (reverse_iterator const &obj) const 	{ return value != obj.value; };
-		bool				operator< (reverse_iterator const &obj) const	{ return value > obj.value; };
-		bool				operator> (reverse_iterator const &obj) const 	{ return value < obj.value; };
-		bool				operator<= (reverse_iterator const &obj) const 	{ return value >= obj.value; };
-		bool				operator>= (reverse_iterator const &obj) const 	{ return value <= obj.value; };
+			typename ft::enable_if<std::is_convertible<U, iterator_type>::value>::type* = 0)
+				: value(other.base()) 					{};
+		T			base() const 					{ return value; }
+		reverse_iterator	&operator=(const reverse_iterator &obj) 	{ value = obj.value; return *this; }
+		reverse_iterator	operator++(int) 				{ reverse_iterator tmp(*this); value--; return tmp; }
+		reverse_iterator	operator--(int) 				{ reverse_iterator tmp(*this); value++; return tmp; }
+		reverse_iterator	&operator++() 					{ value--; return *this; }
+		reverse_iterator	&operator--() 					{ value++; return *this; }
+		int			operator-(reverse_iterator const &obj) const 	{ return value + obj.value; }
+		int			operator+(reverse_iterator const &obj) const 	{ return value - obj.value; }
+		reverse_iterator	operator-(int n) const 				{ return reverse_iterator(this->value + n); }
+		reverse_iterator	operator+(int n) const 				{ return reverse_iterator(this->value - n); }
+		reverse_iterator	&operator-=(int n) 				{ this->value += n; return (*this); }
+		reverse_iterator	&operator+=(int n) 				{ this->value -= n; return (*this); }
+		reference 		operator*() const 				{ return *value; }
+		pointer 		operator->() const 				{ return &(*value); }
+		reference 		operator[](const unsigned int index) 		{ return *this->value[index]; }
+		bool			operator== (reverse_iterator const &obj) const 	{ return value == obj.value; };
+		bool			operator!= (reverse_iterator const &obj) const 	{ return value != obj.value; };
+		bool			operator< (reverse_iterator const &obj) const	{ return value > obj.value; };
+		bool			operator> (reverse_iterator const &obj) const 	{ return value < obj.value; };
+		bool			operator<= (reverse_iterator const &obj) const 	{ return value >= obj.value; };
+		bool			operator>= (reverse_iterator const &obj) const 	{ return value <= obj.value; };
 	};
 
 	template <class T>
@@ -143,25 +143,25 @@ namespace ft {
 			}
 		}
 	public:
-		node_iterator(T value = nullptr) : node(value)						{};
-		~node_iterator()													{};
+		node_iterator(T value = nullptr) : node(value)				{};
+		~node_iterator()							{};
 		template <class U> node_iterator(const node_iterator<U>& other,
-				typename ft::enable_if<std::is_convertible<U, T>::value>::type* = 0)
-						: node(other.base()) 								{};
-		T					base() const 									{ return node; }
-		node_iterator	&operator=(const node_iterator &obj) 				{ node = obj.node; return *this; }
-		node_iterator	operator++(int)										{ node_iterator tmp(*this); next(); return tmp; }
-		node_iterator	&operator++() 										{ next(); return *this; }
-		node_iterator	operator--(int)										{ node_iterator tmp(*this); prev(); return tmp; }
-		node_iterator	&operator--() 										{ prev(); return *this; }
-		T& 				operator*() const 									{ return *node; }
-		T			 	operator->() const 									{ return &(*node); }
-		bool			operator==(node_iterator const &obj) const 			{ return node == obj.node; };
-		bool			operator!=(node_iterator const &obj) const 			{ return node != obj.node; };
-		bool 			operator<(node_iterator const &obj) const 			{ return node < obj.node; };
-		bool 			operator>(node_iterator const &obj) const 			{ return node > obj.node; };
-		bool 			operator<=(node_iterator const &obj) const 			{ return node <= obj.node; };
-		bool 			operator>=(node_iterator const &obj) const 			{ return node >= obj.node; };
+			typename ft::enable_if<std::is_convertible<U, T>::value>::type* = 0)
+				: node(other.base()) 					{};
+		T			base() const 			{ return node; }
+		node_iterator		&operator=(const node_iterator &obj) 			{ node = obj.node; return *this; }
+		node_iterator		operator++(int)						{ node_iterator tmp(*this); next(); return tmp; }
+		node_iterator		&operator++() 						{ next(); return *this; }
+		node_iterator		operator--(int)						{ node_iterator tmp(*this); prev(); return tmp; }
+		node_iterator		&operator--() 						{ prev(); return *this; }
+		T& 			operator*() const 					{ return *node; }
+		T			operator->() const 					{ return &(*node); }
+		bool			operator==(node_iterator const &obj) const 		{ return node == obj.node; };
+		bool			operator!=(node_iterator const &obj) const 		{ return node != obj.node; };
+		bool 			operator<(node_iterator const &obj) const 		{ return node < obj.node; };
+		bool 			operator>(node_iterator const &obj) const 		{ return node > obj.node; };
+		bool 			operator<=(node_iterator const &obj) const 		{ return node <= obj.node; };
+		bool 			operator>=(node_iterator const &obj) const 		{ return node >= obj.node; };
 	};
 
 }
