151cc5924f1e5c1e53e97ebc0e399a549ded88d1
Erik <59801674+divinepet@users.noreply.github.com>
2021-11-03 21:22:32 +0300

    Update Set.hpp

--- Set.hpp
+++ Set.hpp
@@ -8,29 +8,29 @@ namespace ft {
 template <class Key, class Compare = std::less<Key>, class A = std::allocator<Key > >
 class Set {
 public:
-	typedef Key										key_type;
-	typedef Key										value_type;
-	typedef std::size_t									size_type;
-	typedef std::ptrdiff_t									difference_type;
-	typedef Compare										key_compare;
-	typedef Compare										value_compare;
-	typedef A										allocator_type;
-	typedef value_type&									reference;
-	typedef const value_type&								const_reference;
-	typedef typename allocator_type::pointer						pointer;
-	typedef typename allocator_type::const_pointer						const_pointer;
-	typedef ft::node_iterator<Node_<value_type>*, value_type>				iterator;
-	typedef ft::node_iterator<const Node_<value_type>*, value_type>				const_iterator;
-	typedef ft::reverse_iterator<iterator>							reverse_iterator;
-	typedef ft::reverse_iterator<const_iterator>						const_reverse_iterator;
-	typedef typename allocator_type::template rebind<Node_<value_type> >::other		allocator_rebind_node;
-	typedef typename allocator_type::template rebind<Tree<value_type> >::other		allocator_rebind_tree;
+	typedef Key											key_type;
+	typedef Key											value_type;
+	typedef std::size_t										size_type;
+	typedef std::ptrdiff_t										difference_type;
+	typedef Compare											key_compare;
+	typedef Compare											value_compare;
+	typedef A											allocator_type;
+	typedef value_type&										reference;
+	typedef const value_type&									const_reference;
+	typedef typename allocator_type::pointer							pointer;
+	typedef typename allocator_type::const_pointer							const_pointer;
+	typedef ft::node_iterator<Node_<value_type>*, value_type>					iterator;
+	typedef ft::node_iterator<const Node_<value_type>*, value_type>					const_iterator;
+	typedef ft::reverse_iterator<iterator>								reverse_iterator;
+	typedef ft::reverse_iterator<const_iterator>							const_reverse_iterator;
+	typedef typename allocator_type::template rebind<Node_<value_type> >::other			allocator_rebind_node;
+	typedef typename allocator_type::template rebind<Tree<value_type> >::other			allocator_rebind_tree;
 private:
-	A			 								_allocator;
-	allocator_rebind_tree									_allocator_rebind_tree;
-	allocator_rebind_node									_allocator_rebind_node;
-	Compare		 									_comp;
-	Tree<value_type >*									_tree;
+	A			 									_allocator;
+	allocator_rebind_tree										_allocator_rebind_tree;
+	allocator_rebind_node										_allocator_rebind_node;
+	Compare		 										_comp;
+	Tree<value_type >*										_tree;
 public:
 
 	Set() {
@@ -75,19 +75,19 @@ public:
 		clearSet();
 	}
 
-	allocator_type			get_allocator() const					{ return _allocator; }
-	iterator 			begin()							{ return _tree->getBegin(); }
-	const_iterator 			begin() const						{ return _tree->getBegin(); }
-	iterator 			end()							{ return _tree->getEnd(); }
-	const_iterator 			end() const						{ return _tree->getEnd(); }
-	reverse_iterator 		rbegin()						{ return iterator(_tree->getLast()); }
-	const_reverse_iterator 		rbegin() const						{ return const_iterator(_tree->getLast()); }
-	reverse_iterator 		rend()							{ return iterator(_tree->getEnd()); }
-	const_reverse_iterator 		rend() const						{ return const_iterator(_tree->getEnd()); }
-	bool 				empty() const						{ return size() == 0; }
-	size_type			size() const 						{ return _tree->m_size; }
-	size_type			max_size() const 					{ return std::numeric_limits<size_type>::max()
-													/ sizeof(Node_<value_type>); }
+	allocator_type			get_allocator() const						{ return _allocator; }
+	iterator 			begin()								{ return _tree->getBegin(); }
+	const_iterator 			begin() const							{ return _tree->getBegin(); }
+	iterator 			end()								{ return _tree->getEnd(); }
+	const_iterator 			end() const							{ return _tree->getEnd(); }
+	reverse_iterator 		rbegin()							{ return iterator(_tree->getLast()); }
+	const_reverse_iterator 		rbegin() const							{ return const_iterator(_tree->getLast()); }
+	reverse_iterator 		rend()								{ return iterator(_tree->getEnd()); }
+	const_reverse_iterator 		rend() const							{ return const_iterator(_tree->getEnd()); }
+	bool 				empty() const							{ return size() == 0; }
+	size_type			size() const 							{ return _tree->m_size; }
+	size_type			max_size() const 						{ return std::numeric_limits<size_type>::max()
+														/ sizeof(Node_<value_type>); }
 
 	void clear() {
 		clearSet();
